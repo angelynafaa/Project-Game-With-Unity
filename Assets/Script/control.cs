a793fe449646a1347c68e6208c68fd9f9ea1c51c
angelynafaa <angelyna826@gmail.com>
2021-01-19 16:56:53 +0700

    initial commit

---
+++ Assets/Script/control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    float x,y;
+    public GameObject huruf;
+    public int kontrol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x= transform.localScale.x;
+        y= transform.localScale.y;
+    }
+    void OnMouseDown(){
+        GetComponent<AudioSource> ().Play();
+        transform.localScale = new Vector2 (x*1.2f, y/1.2f);
+    }
+    void OnMouseUp(){
+        transform.localScale = new Vector2(x,y);
+        huruf.GetComponent<huruf>().control (kontrol);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
